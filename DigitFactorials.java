6b90c31d8cade645c7182b3d5e6d06d86c8eaf0d
Andrew H. Pometta <apomet@gmail.com>
2015-12-01 16:04:35 -0800

    DigitFactorials.java

---
+++ DigitFactorials.java
@@ -0,0 +1,20 @@
+/*This small Java program, written by Andrew H. Pometta, is made to solve
+  project euler question number 34: digit factorials.  For more information
+  on the mathematical reasoning and problem description, see the README.
+*/
+
+import Factorials.java
+
+public class DigitFactorials {
+    //See the README for why our max is known to be 2,540,160 and our min is 10
+    final int MAX = 2540160;
+
+    public static void main(String[] args) {
+        Factorials facts = new Factorials();
+        int sum = 0;
+        for (int i = 10; i < MAX; ++i){
+            if (facts.digit_check(i)) sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
