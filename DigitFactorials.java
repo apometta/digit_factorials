4d9f1b7309b14f29c143d5700f3f8922b16ed4d3
Andrew H. Pometta <apomet@gmail.com>
2015-12-01 16:25:53 -0800

    DigitFactorials.java

--- DigitFactorials.java
+++ DigitFactorials.java
@@ -3,11 +3,9 @@
   on the mathematical reasoning and problem description, see the README.
 */
 
-import Factorials.java
-
 public class DigitFactorials {
     //See the README for why our max is known to be 2,540,160 and our min is 10
-    final int MAX = 2540160;
+    final static int MAX = 2540160;
 
     public static void main(String[] args) {
         Factorials facts = new Factorials();
